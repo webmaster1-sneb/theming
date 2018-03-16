e4469f8dabd416f474829a6789e691802875e759
uid88437 <uid88437@h2web105.infomaniak.ch>
2018-03-16 08:33:39 +0100

    initial commit

---
+++ themes/sneb-2018/README.md
@@ -0,0 +1,7 @@
+# Sneb 2018 Theme
+
+The **Sneb 2018** Theme is for [Grav CMS](http://github.com/getgrav/grav).  This README.md file should be modified to describe the features, installation, configuration, and general usage of this theme.
+
+## Description
+
+Responsive theme for SNEB
---
+++ themes/sneb-2018/js/windguru.js
@@ -0,0 +1,56 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//<![CDATA[
+    // NIDAU ->
+ WgWidget({
+        s: 56996, odh:6, doh:22, wj:'knots', tj:'c', waj:'m', fhours:72, lng:'fr',
+        params: ['WINDSPD','GUST','SMER','TMPE','CDC','APCPs'],
+        first_row:true,
+        spotname:true,
+        first_row_minfo:false,
+        last_row:true,
+        lat_lon:false,
+        tz:false,
+        sun:true,
+        link_archive:false,
+        link_new_window:false
+    },
+    'wg_target_div_56996_18795452'
+);
+ 
+    //LA NEUVILLE -->
+    WgWidget({
+        s: 17495, odh:6, doh:22, wj:'knots', tj:'c', waj:'m', fhours:72, lng:'fr',
+        params: ['WINDSPD','GUST','SMER','TMPE','CDC','APCPs'],
+        first_row:true,
+        spotname:true,
+        first_row_minfo:false,
+        last_row:true,
+        lat_lon:false,
+        tz:false,
+        sun:true,
+        link_archive:false,
+        link_new_window:false
+    },
+    'wg_target_div_17495_10105469'
+);
+    // Lüscherz
+    WgWidget({
+        s: 56994, odh:6, doh:22, wj:'knots', tj:'c', waj:'m', fhours:72, lng:'fr',
+        params: ['WINDSPD','GUST','SMER','TMPE','APCPs'],
+        first_row:true,
+        spotname:true,
+        first_row_minfo:false,
+        last_row:true,
+        lat_lon:false,
+        tz:false,
+        sun:true,
+        link_archive:false,
+        link_new_window:false
+    },
+    'wg_target_div_56994_28069874'
+);
+
